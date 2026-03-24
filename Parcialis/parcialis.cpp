fa949cf565fef8bda39cc3ff91bdc2baffadcc7c
ferenczakos-cmd <ferencz.akos@student.ms.sapientia.ro>
2026-03-24 10:13:58 +0200

    10:13

--- Parcialis/parcialis.cpp
+++ Parcialis/parcialis.cpp
@@ -6,9 +6,46 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+const int intCMP(const void*a,const void*b ) {
+    int arg1 = *(int*)a;
+    int arg2 = *(int*)b;
+    return arg1-arg2;
+}
+
 using namespace std;
+// cukorkak -> moho modszert fogok alkalmazni
+void I() {
+    // elosszor beolvassuk az adatokat
+    ifstream f;
+    f.open("cukorka.txt");
+    int n,m;
+    f>>n;
+    cout<<n<<endl;
+    // feltetelezek max 100 gyereket
+    int x[100]={0};
+    for(int i=0;i<n;i++) {
+        f>>x[i];
+        cout<<x[i]<<" ";
+    }
+    f>>m;
+    cout<<endl<<m<<endl;
+    // feltetelezek max 100 cukorkat
+    int y[100]={0};
+    for(int i=0;i<m;i++) {
+        f>>y[i];
+        cout<<y[i]<<" ";
+    }
+    f.close();
+    // rendezni kell a tomboket
+    qsort(x,m,sizeof(int),intCMP());
+    // moho megoldas alkalmazasa
+}
+// penz -> backtracking kell
+void II() {
+
+}
 
 int main() {
-    cout<<"Hello World!"<<endl;
+    I();
     return 0;
 }
