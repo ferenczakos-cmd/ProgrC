5d04b58df77f5d0ac4fb8188eee87b6f7412f139
ferenczakos-cmd <ferencz.akos@student.ms.sapientia.ro>
2026-03-24 10:26:06 +0200

    10:26

--- Parcialis/parcialis.cpp
+++ Parcialis/parcialis.cpp
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-const int intCMP(const void*a,const void*b ) {
+ int intCMP(const void*a,const void*b ) {
     int arg1 = *(int*)a;
     int arg2 = *(int*)b;
     return arg1-arg2;
@@ -15,6 +15,8 @@ const int intCMP(const void*a,const void*b ) {
 using namespace std;
 // cukorkak -> moho modszert fogok alkalmazni
 void I() {
+    cout<<"A beolvasott adatok:"<<endl;
+
     // elosszor beolvassuk az adatokat
     ifstream f;
     f.open("cukorka.txt");
@@ -36,9 +38,42 @@ void I() {
         cout<<y[i]<<" ";
     }
     f.close();
+
     // rendezni kell a tomboket
-    qsort(x,m,sizeof(int),intCMP());
+    qsort(x,n,sizeof(int),intCMP);
+    qsort(y,m,sizeof(int),intCMP);
+
+    //Teszteles miatt kiiratom a tomboket ujbol
+    cout<<endl<<"A rendezett tombok:"<<endl;
+    cout<<n<<endl;
+    for(int i=0;i<n;i++) {
+        cout<<x[i]<<" ";
+    }
+    cout<<endl<<m<<endl;
+    for(int i=0;i<m;i++) {
+        cout<<y[i]<<" ";
+    }
+
     // moho megoldas alkalmazasa
+    int cukorkaindex=0;
+    int boldogGyerek=0;
+    //vegigmegyunk a gyerekek listajan
+    for(int i=0;i<n;i++) {
+        //keressuk a megfelelo cukorkat, ha meg van nagyobb cukorka
+        if (cukorkaindex<m) {
+            for(int j=cukorkaindex;j<m;j++) {
+                if (x[i]<=y[j]) {
+                    // ez utan csak a nagyobb cukorkakat nezzuk
+                    cukorkaindex=j+1;
+                    boldogGyerek++;
+                    // haladjuk tovabb a kovetkezo gyerekre
+                    break;
+                }
+            }
+        }
+
+    }
+    cout<<endl<<"A boldog gyerekek szama: "<<boldogGyerek<<endl;
 }
 // penz -> backtracking kell
 void II() {
