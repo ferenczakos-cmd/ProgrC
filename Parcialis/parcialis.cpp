060549e91a59a04ea0715ec04f34168ab4659a0e
ferenczakos-cmd <ferencz.akos@student.ms.sapientia.ro>
2026-05-12 10:18:30 +0300

    Parcialis_10:18

--- Parcialis/parcialis.cpp
+++ Parcialis/parcialis.cpp
@@ -7,9 +7,37 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "../header/queue.h"
 
+using namespace std;
+
+struct szoba {
+    int szomszedszam;
+    Queue SzomszedSzobak;
+    bool latogatott=false;
+};
 
 int main() {
 
+
+    ifstream f;
+    f.open("kastely.txt");
+    if (!f) {
+        cout<<"Fajl olvasasi hiba...";
+        return 1;
+    }
+    int a,b;
+    f>>a>>b;
+    cout<<a<<" "<<b<<endl;
+    szoba Szobak[a];
+
+    for (int i=0;i<a;i++) {
+        Szobak[i].szomszedszam=0;
+        createQueue(5,&Szobak[i].SzomszedSzobak);
+    }
+    for (int i=0;i<b;i++) {
+        int x,y;
+
+    }
     return 0;
 }
